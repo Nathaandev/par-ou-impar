9f7a6dc997eddaf93d6c69caa3f6910e145c03cf
Nathan da Silva <111094286+Nathaandev@users.noreply.github.com>
2023-03-01 17:06:51 -0300

    Add files via upload

---
+++ par ou ímpar.py
@@ -0,0 +1,10 @@
+numero=int(input('Escolha um número '))
+calc = numero % 2
+
+if calc == 0:
+
+    print('O número {} é um numero par!' .format(numero))
+
+else:
+
+    print('O número {} é um numero ímpar!' .format(numero))
